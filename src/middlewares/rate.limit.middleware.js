7d6c34bc2d150ed8cee0a2802ae85a79f8602f52
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-04 21:58:34 +0600

    add AppError constructor in signUpLimitMiddleware function in rate.limit.middleware.js file

--- src/middlewares/rate.limit.middleware.js
+++ src/middlewares/rate.limit.middleware.js
@@ -2,6 +2,7 @@
 import RedisStore from 'rate-limit-redis';
 import rateLimit from 'express-rate-limit';
 import redisClient from '../config/config.redis.js';
+import AppError from '../utils/constants/app.error.js';
 
 // sign-up limit middleware ---------------------------->
 const signUpLimitMiddleware = rateLimit({
@@ -10,7 +11,14 @@ const signUpLimitMiddleware = rateLimit({
     store: new RedisStore({
         sendCommand: (...args) => redisClient.sendCommand(args),
     }),
-    message: 'Too many requests. Please try again later.',
+    handler: (req, res, next) => {
+        next(
+            new AppError('Too many requests. Please try again later.', {
+                status: 429,
+                code: 'RATE_LIMIT',
+            })
+        );
+    },
 });
 
 // sign-in limit middleware ---------------------------->
