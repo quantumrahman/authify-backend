e01b590846e5740f5c0ff4f8a048609a17b13142
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-05 00:21:27 +0600

    implement generateOtp function in generator.func.js file

--- src/utils/generator/generator.func.js
+++ src/utils/generator/generator.func.js
@@ -41,3 +41,17 @@ export const generateHashOtp = (otp) => {
 
     return hashedOtp;
 };
+
+// otp generator --------------------------------------->
+export const generateOtp = (length = 6) => {
+    if (length <= 0) {
+        throw new Error('OTP length must be greater than 0.');
+    }
+
+    const min = 10 ** (length - 1);
+    const max = 10 ** length;
+
+    const otp = crypto.randomInt(min, max);
+
+    return otp.toString();
+};
