acbef3597ab828d74301efa6efaf150e1b9a5e91
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-05 13:48:23 +0600

    implement nameFormatter function in utils.func.js file

--- src/utils/functions/utils.func.js
+++ src/utils/functions/utils.func.js
@@ -6,14 +6,33 @@ import bcrypt from 'bcrypt';
 export const comparePassword = async (inputPwd, hashedPwd) => {
     try {
         if (typeof inputPwd !== 'string' || typeof hashedPwd !== 'string') {
-            throw new Error('Password must be a string format.')
+            throw new Error('Password must be a string format.');
         }
 
         const isMatch = await bcrypt.compare(inputPwd, hashedPwd);
 
         return isMatch;
     } catch (error) {
-        console.log(chalk.red('Password cannot compare properly.'), error.message);
+        console.log(
+            chalk.red('Password cannot compare properly.'),
+            error.message
+        );
         throw error;
     }
+};
+
+// formatter function ---------------------------------->
+export const nameFormatter = (name) => {
+    if (typeof name !== 'string') {
+        throw new Error('Name must be a string format.');
+    }
+
+    const nameFormat = name
+        .toLowerCase()
+        .split(' ')
+        .filter((empty) => empty !== '')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+
+    return nameFormat;
 };
