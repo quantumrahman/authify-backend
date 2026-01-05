aba6c845934a74e76e052c0f11a1d6736694a7b4
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-01-05 14:01:56 +0600

    implement validationMiddleware in zod.validation.middleware.js file

---
+++ src/middlewares/zod.validation.middleware.js
@@ -0,0 +1,31 @@
+// import module --------------------------------------->
+import AppError from '../utils/constants/app.error.js';
+
+// validation middleware ------------------------------->
+const validationMiddleware = (schema) => async (req, res, next) => {
+    const result = await schema.safeParseAsync(req.body);
+
+    if (!result.success) {
+        const { fieldErrors } = result.error.flatten();
+
+        return next(
+            new AppError('Validation Error.', {
+                success: result.success,
+                status: 400,
+                code: 'VALIDATION_ERROR',
+                details: Object.entries(fieldErrors).map(
+                    ([field, message]) => ({
+                        field: field,
+                        message: message[0],
+                    })
+                ),
+            })
+        );
+    }
+
+    req.body = result.data;
+    return next();
+};
+
+// export module --------------------------------------->
+export default validationMiddleware;
